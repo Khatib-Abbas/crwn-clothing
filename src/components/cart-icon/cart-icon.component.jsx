c5409494699a346c2fe698ae8c67e4361e322aba
abbas <abbas_khatib@hotmail.com>
2021-08-17 09:56:47 +0200

    moved our directory and shop data into its own reducer, create respective selectors and updated corresponding components with redux flow

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect'
+import { createStructuredSelector } from 'reselect';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import {selectCartItemsCount} from "../../redux/cart/cart.selectors";
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden ,itemCount }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
     <span className='item-count'>{itemCount}</span>
@@ -20,12 +20,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps =createStructuredSelector(
-    {
-        itemCount : selectCartItemsCount
-    }
-)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
+});
+
 export default connect(
-    mapStateToProps,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
